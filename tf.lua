3f8ea1496e1700fbb262d437463e7c6a9a46ba6d
Viktor Richter <vrichter@posteo.de>
2019-03-15 15:29:10 +0100

    change calculation of rotation

--- tf.lua
+++ tf.lua
@@ -27,10 +27,11 @@ local function transform_2d_pose(pose, tf)
         local point = homogenize(tf * matrix {{pose.x}, {pose.y}, {1}})
         return {x = point[1][1], y = point[2][1]}
     else
-        local point = homogenize(tf * matrix {{pose.x}, {pose.y}, {1}}) -- this is a point
-        local direction = homogenize(tf * matrix {{ math.cos(pose.rad) }, { math.sin(pose.rad) }, { 0 }}) -- this is a vector
-        local norm = matrix.normf(direction)
-        local rotation = math.atan2(direction[2][1]/norm, direction[1][1]/norm);
+        local point = homogenize(tf * matrix {{pose.x}, {pose.y}, {1}})
+        local dir_point0 = homogenize(tf * matrix {{ 0 }, { 0 }, { 1 }})
+        local dir_point1 = homogenize(tf * matrix {{ 0 }, { 1 }, { 1 }})
+        local dir_point3 = dir_point1 - dir_point0
+        local rotation = math.atan2(dir_point3[1][1],dir_point3[2][1]) + pose.rad
         return {x = point[1][1], y = point[2][1], rad = rotation}
     end
 end
@@ -42,15 +43,19 @@ function Tf:transform_to_home(track)
 end
 function Tf:transform_to(track, frame_id)
     if (not (track.frame_id)) or (not (frame_id)) or (track.frame_id == frame_id)  then return track end
-    if not (track.frame_id == 'Home') then
-        msg.info('transforming track',dump(track),'from"'..frame_id..'" to "Home"')
-        track = self:transform_to_home(track)
-        msg.info('result:',dump(track))
+    local home = matrix {{1,0,0},{0,1,0},{0,0,1}}
+    local from = 'Home'
+    if not (track.frame_id == from) then
+        --msg.info('transforming track',dump(track),'from"'..frame_id..'" to "Home"')
+        --track = self:transform_to_home(track)
+        --msg.info('result:',dump(track))
+        home = matrix.invert(self.transformations[track.frame_id])
+        from = track.frame_id
     end
     if self.transformations[frame_id] then
-        msg.info('transforming track',dump(track),'from "Home" to "'..frame_id..'"')
-        local result = transform_2d_pose(track,self.transformations[frame_id])
-        msg.info('result:',dump(result))
+        --msg.info('transforming track',dump(track),'from "' .. from .. '" to "'..frame_id..'"')
+        local result = transform_2d_pose(track,self.transformations[frame_id] * home)
+        --msg.info('result:',dump(result))
         result.frame_id = frame_id
         return result
     end
